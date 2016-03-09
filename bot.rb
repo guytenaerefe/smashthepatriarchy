396c0359c30ad93a4b0275d333e4c0c212686007
Francesco de Guytenaere <guytenaere.fe@gmail.com>
2016-03-09 23:11:59 +0100

    Added saving done tweet id to file, check reading from file to see if tweet is done

--- bot.rb
+++ bot.rb
@@ -2,9 +2,50 @@
 require 'twitter'
 require 'yaml'
 
+class Matriarch
+
+    def initialize(config, client)
+        @config = config
+        @client = client
+    end
+
+    # Debugging
+    def test_done
+        puts check_done('707682618209341440z')
+    end
+
+    # Append ID to file 
+    def update_done(id)
+        open('tweets.done', 'a') { |f|
+          f.puts(id)
+        }
+    end
+
+    # Check 
+    def check_done(id)
+        return false unless        
+            File.foreach('tweets.done') do |line|
+                return true if line.chomp == id.to_s
+            end
+    end
+
+    def watch
+        counter = 0
+        loop do
+            tweet = @client.search("businesswoman").first
+            update_done(tweet.id)
+            #rand_time = rand(120*60..240*60)
+            rand_time = rand(10..300)
+            puts tweet.text
+            puts 'Tweeted, now resting for a while'
+            sleep rand_time
+        end
+    end
+
+end
+
 config = YAML.load_file('config.yml')
+client = client = Twitter::REST::Client.new(config['twitter_credentials'])
 
-client = Twitter::REST::Client.new(config['twitter_credentials'])
-
-test = client.search("businesswoman").first.text
-puts test
+spill_it = Matriarch.new(config, client)
+spill_it.test_done
