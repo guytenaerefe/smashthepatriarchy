c4c6cf55441ce930ba21147c97add6ba09df9712
Francesco de Guytenaere <guytenaere.fe@gmail.com>
2016-03-10 00:22:10 +0100

    removed twitter data for test. Need to find a way to mock it

--- bot.test.rb
+++ bot.test.rb
@@ -5,9 +5,7 @@ require './bot'
 
 class TC_Bot < Test::Unit::TestCase
     def test_win
-        config = YAML.load_file('config.yml')
-        client = client = Twitter::REST::Client.new(config['twitter_credentials'])        
-        test_it = Matriarch.new(config, client)
+        test_it = Matriarch.new({}, {})
         assert(test_it.check_done(707682618209341440), 'Item does exist')
     end
 end
