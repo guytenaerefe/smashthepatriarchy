ffe5720a1dcc03e798c5c3d272f336d61a02f7a9
Francesco de Guytenaere <guytenaere.fe@gmail.com>
2016-03-10 10:58:42 +0100

    Added another puny test

--- bot.test.rb
+++ bot.test.rb
@@ -5,8 +5,17 @@ require 'yaml'
 require './bot'
 
 class TC_Bot < Test::Unit::TestCase
-    def test_win
+
+    def test_key_already_handled_
         test_it = Matriarch.new({}, {})
         assert(test_it.check_done(707682618209341440), 'Item does exist')
+
+    end
+
+    def test_retrieve_valid_key_value
+        test_it = Matriarch.new({}, {})
+        word = test_it.random_word
+        assert_not_nil(word.bad)
+        assert_not_nil(word.good)
     end
 end
