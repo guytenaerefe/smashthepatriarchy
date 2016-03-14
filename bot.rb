dea4fbb1319092b50504a31fddc1a688d86c6916
Francesco de Guytenaere <guytenaere.fe@gmail.com>
2016-03-14 14:38:47 +0100

    * Refactored watch-loop * Error catching

--- bot.rb
+++ bot.rb
@@ -53,36 +53,43 @@ class Matriarch
         return reply_line.chomp
     end
 
+    def output_sleep(min,max,text)
+        rand_time = rand(min..max)
+        time = Time.new
+        puts '--- '+ text +' (' + (rand_time/60).to_s + 'm at ' + time.inspect + ') ---'
+        sleep rand_time
+    end
+
     # Main loop
     def watch
         counter = 0
         loop do
-            word = random_word
-            tweet = @client.search(word.bad + ' -rt', lang: "en").first
-            if !check_done(tweet.id) && counter < 50 && tweet.user.screen_name != 'don_quibot'
-                reply = random_line(tweet.user.screen_name, word.bad, word.good)
-                counter += 1
-                update_done(tweet.id) 
-                @client.update(reply, in_reply_to_status_id: tweet.id)
-                puts tweet.text
-                puts reply
-                rand_time = rand(180..900)
-                time = Time.new
-                puts '--- Tweeted, now resting for a while (' + (rand_time/60).to_s + 'm at ' + time.inspect + ') ---'
-                sleep rand_time
-            elsif counter > 50 
-                counter = 0
-                rand_time = rand(1800..3600)
-                time = Time.new                
-                puts '--- Done a shitload of tweets, let\'s try another in a while (' + (rand_time/60).to_s + 'm at ' + time.inspect + ') ---'                
-                sleep rand_time
-            else 
-                rand_time = rand(180..360)
-                time = Time.new
-                puts '--- Done this tweet (' + tweet.text + ') already, let\'s try another in a while (' + (rand_time/60).to_s + 'm at ' + time.inspect + ') ----'                
-                sleep rand_time
+            begin
+                word = random_word
+                tweet = @client.search(word.bad + ' -rt', lang: "en").first
+                if !check_done(tweet.id) && counter < 50 && tweet.user.screen_name != 'don_quibot'
+                    reply = random_line(tweet.user.screen_name, word.bad, word.good)
+                    counter += 1
+                    update_done(tweet.id)
+                    @client.update(reply, in_reply_to_status_id: tweet.id)
+                    puts tweet.text
+                    puts reply
+                    output_sleep(180,900, 'Tweeted. Taking a rest')
+                elsif counter > 50
+                    counter = 0
+                    output_sleep(1800, 3600, 'Done a shitload of tweets, let\'s try another in a while')
+                else 
+                    output_sleep(180, 360, 'Done this tweet (' + tweet.text + ') already, let\'s try another in a while')
+                end
+            rescue Errno::ETIMEDOUT
+                puts "Operation timed out. Retrying"
+                retry
+            rescue Errno::ECONNRESET
+                puts "Connection reset by peer. Retrying"
+                retry                    
+            rescue Twitter::Error::Forbidden
+                puts "Twitter:Forbidden. Probably over 140 characters"
             end
         end
     end
-
 end
